d1c9f39d0cdf763a2ae434e15f25ff602ef14615
Neutron-Inv <jackomega.idnoble@gmail.com>
2025-02-16 11:38:57 +0100

    landing page for desktop done

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,40 +4,54 @@
             <img src="{{asset('/storage/images/logo.png')}}" class="h-32 object-contain justify-self-start">
             we deliver accuracy you can trust...
         </p>
-        <span class="text-sm text-gray-400 justify-self-end"> Designed by Virtual Horizons </span>
     </div>
-    <div class="grid grid-cols-4 items-start">
+    <div class="grid grid-cols-4 justify-items-center">
         <div>
-        <p class="font-bold text-md py-4"> Company</p>
-        <ul class="font-medium text-sm text-gray-400 leading-6"> 
-            <li> <a> About Us </a> </li>
-            <li> Products </li>
+            <p class="font-bold text-md py-4"> Our Office</p>
+            <ul class="font-medium text-sm text-gray-400 leading-6 space-y-2"> 
+                <li> Huson House, Emmanuel Emenike Road,
+                    By Court of Appeal, Opposite Crown Plaza Hotel
+                    Area A, New Owerri, Imo State. </li>
+                <li> <a href="tel:+234860936649">+234860936649</a>, <a href="tel:+2348147182326">+2348147182326</a> </li>
+                <li> <a href="mailto:seamlesssmartconsult@gmail.com">seamlesssmartconsult@gmail.com</a> </li>
+            </ul>
+        </div>
+
+
+        <div>
+        <p class="font-bold text-md py-4"> Quick Links</p>
+        <ul class="font-medium text-sm text-gray-400 leading-6 space-y-2"> 
+            <li> <a> Home </a> </li>
+            <li> About Us </li>
+            <li> Services </li>
+            <li> FAQs </li>
+            <li> Contact Us </li>
         </ul>
         </div>
 
         <div>
             <p class="font-bold text-md py-4"> Services</p>
-            <ul class="font-medium text-sm text-gray-400 leading-6"> 
-                <li> About Us </li>
-                <li> Products </li>
+            <ul class="font-medium text-sm text-gray-400 leading-6 space-y-2"> 
+                <li> Land & Aerial Surveying </li>
+                <li> GIS & Mapping Solutions </li>
+                <li> Bathymetry Solutions </li>
+                <li> Remote Sensing & Drone Mapping </li>
+                <li> Geospatial Data Analysis </li>
             </ul>
         </div>
 
         <div>
-            <p class="font-bold text-md py-4"> Services</p>
-            <ul class="font-medium text-sm text-gray-400 leading-6"> 
-                <li> About Us </li>
-                <li> Products </li>
+            <p class="font-bold text-md py-4"> Stay Connected</p>
+            <ul class="font-medium text-sm text-gray-400 leading-6 space-y-2"> 
+                <li class="flex items-center space-x-3">
+                    <i class="fa-brands fa-facebook-f text-gray-700 text-2xl bg-green-800 bg-clip-text text-transparent"></i>
+                    <i class="fa-brands fa-linkedin text-gray-700 text-3xl bg-green-800 bg-clip-text text-transparent"></i>
+                    <i class="fa-brands fa-instagram text-gray-700 text-3xl bg-green-800 bg-clip-text text-transparent"></i>
+                </li>
+                <li> Follow us on social media for updates, insights, and real-time engagement with our team.</li>
             </ul>
         </div>
 
-        <div>
-            <p class="font-bold text-md py-4"> Resource</p>
-            <ul class="font-medium text-sm text-gray-400 leading-6"> 
-                <li> About Us </li>
-                <li> Products </li>
-            </ul>
-        </div>
 
         
     </div>
