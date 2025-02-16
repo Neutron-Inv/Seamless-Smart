2af7277cebe63534e1479c4ec3a0398d62bef4ca
Neutron-Inv <jackomega.idnoble@gmail.com>
2025-02-16 10:40:08 +0100

    landing page 70% completed

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <div class="bg-cover bg-center px-20 py-4 space-y-14 font-raleway" data-aos="fade-up" data-aos-duration="800" data-aos-delay="400" style="background-image: url('{{ asset('/storage/images/footerbg.png') }}');">
     <div class="grid grid-cols-2 gap-10 justify-items-stretch items-center">
-        <p class="font-thin text-sm">
+        <p class="font-thin text-sm text-gray-500">
             <img src="{{asset('/storage/images/logo.png')}}" class="h-32 object-contain justify-self-start">
             we deliver accuracy you can trust...
         </p>
