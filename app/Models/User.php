0e3a247def0f43957d67f1677d9c40fe6b84cd43
Neutron-Inv <jackomega.idnoble@gmail.com>
2025-03-16 19:30:03 +0100

    first launch version

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Filament\Panel;
 use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
 class User extends Authenticatable implements FilamentUser
 {
@@ -48,8 +48,8 @@ class User extends Authenticatable implements FilamentUser
         ];
     }
 
-    public function canAccessFilament(): bool
+    public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@xxx.com');
+        return str_ends_with($this->email, '@seamlesssmart.com') && $this->hasVerifiedEmail();
     }
 }
